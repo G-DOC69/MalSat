3525cd718b89d41cd2be18cb2f038eafc6b2c4b6
linkuello <144430394+linkuello@users.noreply.github.com>
2025-05-21 06:41:01 +0600

    Update api.js

--- src/app/api.js
+++ src/app/api.js
@@ -1,7 +1,8 @@
 // api.js
 import axios from 'axios';
 
-axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api`;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
 
 const setAuthToken = (token) => {
     if (token) {
